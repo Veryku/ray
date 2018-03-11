80e2706e82ae8c53ee818a27eea583303dcba2e8
Kunal Gosar <gosarkunal9@gmail.com>
2018-03-10 22:24:25 -0800

    rename cols, rows to _col_partitions and _row_partitions

--- python/ray/dataframe/io.py
+++ python/ray/dataframe/io.py
@@ -45,7 +45,7 @@ def read_parquet(path, engine='auto', columns=None, **kwargs):
         [_split_df.remote(df, chunksize) for df in df_from_row_groups])
     df_remotes = list(chain.from_iterable(splited_dfs))
 
-    return DataFrame(rows=df_remotes, columns=columns)
+    return DataFrame(row_partitions=df_remotes, columns=columns)
 
 
 @ray.remote
@@ -259,4 +259,4 @@ def read_csv(filepath,
                 filepath, start, end, kwargs=kwargs)
         df_obj_ids.append(df)
 
-    return DataFrame(rows=df_obj_ids, columns=columns)
+    return DataFrame(row_partitions=df_obj_ids, columns=columns)
